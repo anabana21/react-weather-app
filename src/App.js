4319a56aaca67fcff6423ec6d1dd1bc70f01f7b3
anabana21 <157493260+anabana21@users.noreply.github.com>
2024-07-24 16:50:50 +0200

    added basic styling

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from "react";
-
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1> Weather App</h1>
+      <div className="container">
+        <Weather defaultCity="New York" />
 
       <footer>
-        This project was coded by 
-        <a href="https://github.com/anabana21" target="_blank" rel="noreferrer">Ana Vogt</a> and is 
-        <a href="https://github.com/anabana21/react-weather-app" target="_blank" rel="noreferrer">open-sourced on Github</a> and hosted on 
-        <a href="https://willowy-dusk-b8e010.netlify.app/" target="_blank" rel="noreferrer">Netlify</a></footer>
+        This project was coded by
+        <a href="https://github.com/anabana21" target="_blank" rel="noreferrer"> Ana Vogt</a> and is 
+        <a href="https://github.com/anabana21/react-weather-app" target="_blank" rel="noreferrer"> open-sourced on Github</a> and hosted on 
+        <a href="https://willowy-dusk-b8e010.netlify.app/" target="_blank" rel="noreferrer"> Netlify</a>
+        </footer>
+        </div>
     </div>
-  ); }
+    );
+    }
