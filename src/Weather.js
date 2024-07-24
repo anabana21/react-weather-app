28af36be35a732f5c269db5a5c4f3cdd072e147f
anabana21 <157493260+anabana21@users.noreply.github.com>
2024-07-24 17:49:57 +0200

    Added API integration

--- src/Weather.js
+++ src/Weather.js
@@ -1,47 +1,69 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from "axios";
 import "./Weather.css";
 
-export default function Weather () {
-    return (
-        <div className="Weather">
-            <form>
-                <div className="row">
-                    <div className="col-9">
-                <input type="search" placeholder="Enter a City..." className="form-control"
-                autoFocus="on"/>
-                
-                </div>
-            <div className="col-3">
+export default function Weather(props) {
+    const [weatherData, setWeatherData] = useState({ ready: false });
+    const [city, setCity] = useState(props.defaultCity);
 
-            
-            <input type="submit" value="Search" className="btn btn-primary w-100"/>
-            
-            </div>
-            </div>
+    function handleResponse(response) {
+        setWeatherData({
+          ready: true,
+          coordinates: response.data.coord,
+          temperature: response.data.main.temp,
+          humidity: response.data.main.humidity,
+          date: new Date(response.data.dt * 1000),
+          description: response.data.weather[0].description,
+          icon: response.data.weather[0].icon,
+          wind: response.data.wind.speed,
+          city: response.data.name,
+        });
+      }
+
+      function handleSubmit(event) {
+        event.preventDefault();
+        search();
+      }
+
+      function handleCityChange(event) {
+        setCity(event.target.value);
+      }
+
+      function search() {
+        const apiKey = "fab7o44152df89e90853f216t75f2b9a";
+        let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+        axios.get(apiUrl).then(handleResponse);
+      }
+
+      if (weatherData.ready) {
+        return (
+          <div className="Weather">
+            <form onSubmit={handleSubmit}>
+              <div className="row">
+                <div className="col-9">
+                  <input
+                    type="search"
+                    placeholder="Enter a city.."
+                    className="form-control"
+                    autoFocus="on"
+                    onChange={handleCityChange}
+                  />
+                </div>
+                <div className="col-3">
+                  <input
+                    type="submit"
+                    value="Search"
+                    className="btn btn-primary w-100"
+                  />
+                </div>
+              </div>
             </form>
-        <h1>New York</h1>
-        <ul>
-            <li>
-                Wednesday 07:00
-            </li>
-            <li>
-                Mostly Cloudy
-            </li>
-        </ul>
-        <div className="row">
-            <div className="col-6">
-                <img src="https://ssl.gstatic.com/onebox/weather/64/fog.png" alt="Mostly Cloudy"/>
-            <span className="temperature">6</span> 
-            <span className="unit">°C</span>
-            </div>
-            <div className="col-6">
-                <ul>
-                    <li>Precipitation: 15%</li>
-                    <li>Humidity; 72%</li>
-                    <li>Wind: 16 km/h</li>
-                </ul>
-            </div>
-        </div>
-        </div>
-    )
-}
+            <WeatherInfo data={weatherData} />
+            <WeatherForecast coordinates={weatherData.coordinates} />
+          </div>
+        );
+      } else {
+        search();
+        return "Loading...";
+      }
+    }
