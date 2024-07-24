847987d351040db92cd74954000f04e40c12b15f
anabana21 <157493260+anabana21@users.noreply.github.com>
2024-07-24 12:39:08 +0200

    Added GitHub Url

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ export default function App() {
   return (
     <div className="App">
       Weather App
+
+      <footer>This project was coded by <a href="https://github.com/anabana21" target="_blank" rel="noreferrer">Ana Vogt</a> and is <a href="https://github.com/anabana21/react-weather-app" target="_blank" rel="noreferrer">open-sourced on Github</a> and hosted on <a href="https://willowy-dusk-b8e010.netlify.app/" target="_blank" rel="noreferrer">Netlify</a> </footer>
     </div>
   );
 }
-
