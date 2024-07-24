14b72615410e503b0cd276721aa7e317e1e80b73
anabana21 <157493260+anabana21@users.noreply.github.com>
2024-07-24 12:57:50 +0200

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React from "react";
 
-import './App.css';
+import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1> Weather App</h1>
 
-      <footer>This project was coded by <a href="https://github.com/anabana21" target="_blank" rel="noreferrer">Ana Vogt</a> and is <a href="https://github.com/anabana21/react-weather-app" target="_blank" rel="noreferrer">open-sourced on Github</a> and hosted on <a href="https://willowy-dusk-b8e010.netlify.app/" target="_blank" rel="noreferrer">Netlify</a> </footer>
+      <footer>
+        This project was coded by 
+        <a href="https://github.com/anabana21" target="_blank" rel="noreferrer">Ana Vogt</a> and is 
+        <a href="https://github.com/anabana21/react-weather-app" target="_blank" rel="noreferrer">open-sourced on Github</a> and hosted on 
+        <a href="https://willowy-dusk-b8e010.netlify.app/" target="_blank" rel="noreferrer">Netlify</a></footer>
     </div>
-  );
-}
+  ); }
