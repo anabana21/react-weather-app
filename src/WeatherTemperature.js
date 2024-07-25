5ba112953e314e59bb53ad6a8c6f2bd7b2988966
anabana21 <157493260+anabana21@users.noreply.github.com>
2024-07-25 13:42:48 +0200

    fixed update for 5 days forecast

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function WeatherTemperature(props) {
   return (
     <div className="WeatherTemperature">
       <span className="temperature">{Math.round(props.celsius)}</span>
-      <span className="unit">°C</span>
+      <span className="unit">
+        °C 
+      </span>
     </div>
   );
 }
