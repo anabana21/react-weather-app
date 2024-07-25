6e6726c76ca487e266b8ce63e75708b417c9b50b
anabana21 <157493260+anabana21@users.noreply.github.com>
2024-07-25 12:59:35 +0200

    Added Forecastday component

--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import WeatherInfo from "./WeatherInfo";
 import WeatherForecast from "./WeatherForecast";
-
 import axios from "axios";
 import "./Weather.css";
 
@@ -62,8 +61,7 @@ export default function Weather(props) {
           </div>
         </form>
         <WeatherInfo data={weatherData} />
-        <WeatherForecast />
-        
+        <WeatherForecast coordinates={weatherData.coordinates} />
       </div>
     );
   } else {
