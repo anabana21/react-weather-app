e0dd94664aa936a3147ebbfc89602d41ea00bb95
anabana21 <157493260+anabana21@users.noreply.github.com>
2024-07-25 13:11:22 +0200

    fixes api code and daily forecast

--- src/Weather.js
+++ src/Weather.js
@@ -32,7 +32,7 @@ export default function Weather(props) {
   }
 
   function search() {
-    const apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    const apiKey = "e6c2364656962bdcb16bc352fc42569a";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
   }
