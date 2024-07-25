d981e62604bd5b5b8d2ccbd611a26568b63f39f2
anabana21 <157493260+anabana21@users.noreply.github.com>
2024-07-25 13:55:30 +0200

    fixed bugs

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ export default function App() {
 
       <footer>
         This project was coded by
-        <a href="https://github.com/anabana21" target="_blank" rel="noreferrer"> Ana Vogt</a> and is 
-        <a href="https://github.com/anabana21/react-weather-app" target="_blank" rel="noreferrer"> open-sourced on Github</a> and hosted on 
-        <a href="https://willowy-dusk-b8e010.netlify.app/" target="_blank" rel="noreferrer"> Netlify</a>
+        <a href="https://github.com/anabana21" target="_blank" rel="noopener noreferrer"> Ana Vogt</a> and is 
+        <a href="https://github.com/anabana21/react-weather-app" target="_blank" rel="noopener noreferrer"> open-sourced on Github</a> and hosted on 
+        <a href="https://willowy-dusk-b8e010.netlify.app/" target="_blank" rel="noopener noreferrer"> Netlify</a>
         </footer>
         </div>
     </div>
